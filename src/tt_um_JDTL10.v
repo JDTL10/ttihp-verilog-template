ca5e45600cd5596c28a728571dac6bb6d68631ed
JDTL10 <jtobarl1@miumg.edu.gt>
2025-06-02 00:19:48 -0600

    Update tt_um_JDTL10.v

--- src/tt_um_JDTL10.v
+++ src/tt_um_JDTL10.v
@@ -1,31 +1,22 @@
-// ===============================
-// tt_um_JDTL10.v (top)
-// ===============================
 module tt_um_JDTL10 (
     input  wire [7:0] ui_in,
     output wire [7:0] uo_out,
-    input  wire [7:0] uio_in,
-    output wire [7:0] uio_out,
-    output wire [7:0] uio_oe,
+    input  wire [7:0] uio_in,   // No usado
+    output wire [7:0] uio_out,  // No usado
+    output wire [7:0] uio_oe,   // No usado
     input  wire       ena,
     input  wire       clk,
     input  wire       rst_n
 );
+    wire [3:0] A = ui_in[3:0];
+    wire [3:0] B = ui_in[7:4];
 
-    // Inputs
-    wire [2:0] OpSel = ui_in[7:5];    // Operación
-    wire [6:0] B = ui_in[6:0];        // B operand
-    wire [6:0] A = uio_in[6:0];       // A operand
-
-    // Outputs
-    wire [6:0] Result;
+    wire [3:0] Result;
     wire CarryOut_ALU, Overflow_ALU, Zero, Negative;
 
-    // ALU instantiation
     ALU alu_inst (
         .A(A),
         .B(B),
-        .OpSel(OpSel),
         .Result(Result),
         .CarryOut_ALU(CarryOut_ALU),
         .Overflow_ALU(Overflow_ALU),
@@ -33,12 +24,12 @@ module tt_um_JDTL10 (
         .Negative(Negative)
     );
 
-    // Map outputs
-    assign uo_out[6:0] = Result;
-    assign uo_out[7]   = CarryOut_ALU;
+    assign uo_out[3:0] = Result;
+    assign uo_out[4]   = CarryOut_ALU;
+    assign uo_out[5]   = Overflow_ALU;
+    assign uo_out[6]   = Zero;
+    assign uo_out[7]   = Negative;
 
-    // Unused outputs
     assign uio_out = 8'b0;
     assign uio_oe  = 8'b0;
-
 endmodule
